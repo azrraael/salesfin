1d518ef73f30bff953f60f2d49a707a152ffc5a3
Jorge Andres Layos Aguirre <layos91@gmail.com>
2023-03-30 22:43:40 -0500

    fin clase 2

--- Sales.Shared/Entities/Country.cs
+++ Sales.Shared/Entities/Country.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Sales.Shared.Entities
 {
-    internal class Country
+    public class Country
     {
+        public int Id { get; set; }
+
+        [Display(Name = "País")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; } = null!;
     }
 }
