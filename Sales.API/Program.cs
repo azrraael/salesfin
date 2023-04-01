5fc26f3bce527546330751b9f3599e456b0670bd
Jorge Andres Layos Aguirre <layos91@gmail.com>
2023-04-01 00:41:41 -0500

    fin parte 16

--- Sales.API/Program.cs
+++ Sales.API/Program.cs
@@ -50,7 +50,7 @@ builder.Services.AddSwaggerGen(c =>
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
 builder.Services.AddTransient<SeedDb>();
 builder.Services.AddScoped<IUserHelper, UserHelper>();
-
+builder.Services.AddScoped<IFileStorage, FileStorage>();
 builder.Services.AddIdentity<User, IdentityRole>(x =>
 {
     x.User.RequireUniqueEmail = true;
