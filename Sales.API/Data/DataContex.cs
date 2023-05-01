2994daf9a02c766ae96108ee7ebfdf886844bf41
Jorge Andres Layos Aguirre <layos91@gmail.com>
2023-05-01 14:58:00 -0500

    fin parte 27

--- Sales.API/Data/DataContex.cs
+++ Sales.API/Data/DataContex.cs
@@ -19,11 +19,18 @@ namespace Sales.API.Data
         public DbSet<Country> Countries { get; set; }
 
         public DbSet<State> States { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        public DbSet<ProductImage> ProductImages { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
             modelBuilder.Entity<Category>().HasIndex(x => x.Name).IsUnique();
+            modelBuilder.Entity<Product>().HasIndex(x => x.Name).IsUnique();
             modelBuilder.Entity<State>().HasIndex("Name", "CountryId").IsUnique();
             modelBuilder.Entity<City>().HasIndex("Name", "StateId").IsUnique();
 
