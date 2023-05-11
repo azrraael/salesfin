06fe3331f675eb9712e9a63e87d6a90446cb7503
Jorge Andres Layos Aguirre <layos91@gmail.com>
2023-05-11 14:24:58 -0500

    fin parte 35

--- Sales.Shared/DTOs/TemporalSaleDTO.cs
+++ Sales.Shared/DTOs/TemporalSaleDTO.cs
@@ -2,10 +2,13 @@
 {
     public class TemporalSaleDTO
     {
+        public int Id { get; set; }
         public int ProductId { get; set; }
 
         public float Quantity { get; set; } = 1;
 
         public string Remarks { get; set; } = string.Empty;
+
+
     }
 }
